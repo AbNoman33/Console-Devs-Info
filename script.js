56e001e48610dadde6a5aebeb4ec81e6fcf0dd4d
Noman <abnoman330@gmail.com>
2022-04-16 23:19:12 +0600

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let student_name = "Abdullah Al Noman";
+let learn = "JavaScript";
+let price = 1000;
+
+
+console.info(`Hello, I am ${student_name}. I am learning ${learn} `);
+console.log(`You can buy it for ${number} taka `);
+
